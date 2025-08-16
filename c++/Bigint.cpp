26b953ee1fdb103de84a363d18e6567fa3162528
gocham <95151738+gochucamchi@users.noreply.github.com>
2025-08-16 23:46:29 +0900

    Create Bigint.cpp

---
+++ c++/Bigint.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+struct Big_int //구조체 생성
+{
+	int Add();
+	int Minus();
+	int Multiplication();//곱셈을 뜻함
+	int Division();
+};
+
+
+int main() {
+  //아직 미완성
+}
+
+int Big_int::Add()
+{
+	cout << 3;
+	return 0;
+}
+
+int Big_int::Minus()
+{
+	return 0;
+}
+
+int Big_int::Multiplication()
+{
+	return 0;
+}
+
+int Big_int::Division()
+{
+	return 0;
+}
